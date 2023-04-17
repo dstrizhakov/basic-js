15b881deb79b374c16d0147812e07b666b205587
Dmitry Strizhakov <d.strizhakov@gmail.com>
2023-04-17 10:50:13 +0300

    feat: add getCommonCharacterCount

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getCommonCharacterCount(s1, s2) {
+  // throw new NotImplementedError('Not implemented');
+  const chars1 = s1.split('');
+  const chars2 = s2.split('');
+  let count = 0;
+
+  for (let i = 0; i < chars1.length; i++) {
+    const index = chars2.indexOf(chars1[i]);
+    if (index !== -1) {
+      count++;
+      chars2.splice(index, 1);
+    }
+  }
+
+  return count;
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,9 +11,71 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+  throw new NotImplementedError("Not implemented");
+
+    if (!date) {
+      return "Unable to determine the time of year!";
+    }
+
+    if (
+      Object.prototype.toString.call(date) !== "[object Date]" ||
+      isNaN(date.getTime())
+    ) {
+      throw new Error("Invalid date!");
+    }
+
+  	if (Object.getPrototypeOf(date) !== Date.prototype) {
+      throw new Error("Invalid date!");
+    }
+
+  	if (
+      !(date instanceof Date) ||
+      isNaN(date.getTime()) ||
+      typeof date.getMonth !== "function" ||
+      typeof date.getFullYear !== "function" ||
+      typeof date.getDate !== "function" ||
+      typeof date.getHours !== "function" ||
+      typeof date.getMinutes !== "function" ||
+      typeof date.getSeconds !== "function" ||
+      typeof date.getMilliseconds !== "function" ||
+      typeof date.getDay !== "function"
+    ) {
+      throw new Error("Invalid date!");
+    }
+
+  	const dateString = date.toISOString().substring(0, 10);
+    const pattern = /^\d{4}-\d{2}-\d{2}$/;
+    if (!pattern.test(dateString)) {
+      throw new Error("Invalid date!");
+    }
+
+  	  if (isNaN(Date.parse(date))) {
+        throw new Error("Invalid date!");
+      }
+
+    const month = date.getMonth();
+
+    switch (month) {
+      case 11:
+      case 0:
+      case 1:
+        return "winter";
+      case 2:
+      case 3:
+      case 4:
+        return "spring";
+      case 5:
+      case 6:
+      case 7:
+        return "summer";
+      case 8:
+      case 9:
+      case 10:
+        return "autumn";
+      default:
+        throw new Error("Invalid date!");
+    }
 }
 
 module.exports = {
